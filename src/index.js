8676a7bc415551cfc52e2cddca4825ea2046ac64
iniestony <shangguanzhanxia@hotmail.com>
2022-06-18 17:26:11 +0800

    add styling approach

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Invitation from './pages/invitation';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Invitation />
   </React.StrictMode>
 );
---
+++ src/pages/invitation/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+function Invitation() {
+  return (
+    <div className={styles.invitation}>
+      test css
+    </div>
+  );
+}
+
+export default Invitation;
